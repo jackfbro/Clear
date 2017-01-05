a437c6f1ec15585b54c4a27ccf3d8876e81d99a0
liujielink <309217279@qq.com>
2017-01-05 23:12:38 +0800

    first

---
+++ Clear/Clear.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clear.Web.MvcApplication" Language="C#" %>
---
+++ Clear/Clear.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clear.Web.Startup))]
+namespace Clear.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
